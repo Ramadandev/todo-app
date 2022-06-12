33d340028647c80a32d101b788d397be6d26bfec
rizkym71338 <rizkym71338@gmail.com>
2022-06-12 21:02:54 +0700

    .

--- src/components/TodoLists.jsx
+++ src/components/TodoLists.jsx
@@ -11,7 +11,9 @@ const TodoLists = () => {
       const afterGetDataFromServer = await getAllDataFromServer.json();
 
       //update state
-      setTodos(afterGetDataFromServer?.todo_list);
+      if (afterGetDataFromServer.message == "Request accepted!") {
+        setTodos(afterGetDataFromServer.todo_list);
+      }
     } catch (err) {
       console.error(err);
     }
@@ -34,27 +36,28 @@ const TodoLists = () => {
 
   return (
     <>
-      {todos?.map((dsc) => (
-        <div
-          className="w-full lg:w-2/5 p-4 h-14 bg-[#262626] mt-9 rounded-xl mx-auto"
-          key={dsc.todos_id}
-        >
-          <div className="flex flex-row">
-            <h1 className="w-2/3 text-white text-md flex-0">
-              {dsc.description}
-            </h1>
-            <button className="flex-1 rounded-md h-7 bg-[#61dafb] mr-2">
-              <i className="fa-solid fa-edit"></i>
-            </button>
-            <button
-              className="flex-1 rounded-md h-7 bg-[#61dafb]"
-              onClick={() => deleteTodo(dsc.todos_id)}
-            >
-              <i className="fa-solid fa-trash"></i>
-            </button>
+      {todos &&
+        todos.map((dsc) => (
+          <div
+            className="w-full lg:w-2/5 p-4 h-14 bg-[#262626] mt-9 rounded-xl mx-auto"
+            key={dsc.todos_id}
+          >
+            <div className="flex flex-row">
+              <h1 className="w-2/3 text-white text-md flex-0">
+                {dsc.description}
+              </h1>
+              <button className="flex-1 rounded-md h-7 bg-[#61dafb] mr-2">
+                <i className="fa-solid fa-edit"></i>
+              </button>
+              <button
+                className="flex-1 rounded-md h-7 bg-[#61dafb]"
+                onClick={() => deleteTodo(dsc.todos_id)}
+              >
+                <i className="fa-solid fa-trash"></i>
+              </button>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
     </>
   );
 };
