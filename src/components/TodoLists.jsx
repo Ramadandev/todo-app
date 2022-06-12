2cb938da0d9e37afa00c87a1ea07f407b972bbc9
rizkym71338 <rizkym71338@gmail.com>
2022-06-12 20:49:31 +0700

    .

--- src/components/TodoLists.jsx
+++ src/components/TodoLists.jsx
@@ -2,47 +2,61 @@ import { useEffect, useState } from "react";
 
 const TodoLists = () => {
   const [todos, setTodos] = useState([]);
-  const getDataTodos = async() => {
-    try {
-      const getAllDataFromServer = await fetch('http://localhost:4000/rest/get-todos');
-      const afterGetDataFromServer = await getAllDataFromServer.json()
-    
-      //update state
-      setTodos(afterGetDataFromServer.todo_list);
-    } catch (error) {
-      console.log(error)
-    }
-  }
 
-  const deleteTodo = async(id) => {
+  const getDataTodos = async () => {
+    try {
+      const getAllDataFromServer = await fetch(
+        "http://localhost:4000/rest/get-todos"
+      );
+      const afterGetDataFromServer = await getAllDataFromServer.json();
+
+      //update state
+      setTodos(afterGetDataFromServer?.todo_list);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const deleteTodo = async (id) => {
     try {
       await fetch(`http://localhost:4000/rest/delete-todos/${id}`, {
-        method: 'DELETE',
-      })
-      window.location = '/';
-    } catch (error) {
-      console.log(error);
+        method: "DELETE",
+      });
+      window.location = "/";
+    } catch (err) {
+      console.error(err);
     }
-  }
+  };
+
   useEffect(() => {
     getDataTodos();
-  }, [])
-  return(
-    <>
-      {
-        todos?.map(dsc => {
-          return <div className="w-full lg:w-2/5  p-4 h-14 bg-[#262626] mt-9 rounded-xl mx-auto" key={dsc.todos_id}>
-            <div className="flex flex-row">
-              <h1 className="text-md flex-0 w-2/3 text-white">{dsc.description}</h1>
-              <button className="flex-1 rounded-md  h-7 bg-[#61dafb] mr-2"><i className="fa-solid fa-edit"></i></button>
-              <button className="flex-1 rounded-md h-7 bg-[#61dafb]" onClick={() => deleteTodo(dsc.todos_id)}><i className="fa-solid fa-trash"></i></button>
-            </div>
-          </div>
-        })
-      }
-    </>
-  )
-}
+  }, []);
 
+  return (
+    <>
+      {todos?.map((dsc) => (
+        <div
+          className="w-full lg:w-2/5 p-4 h-14 bg-[#262626] mt-9 rounded-xl mx-auto"
+          key={dsc.todos_id}
+        >
+          <div className="flex flex-row">
+            <h1 className="w-2/3 text-white text-md flex-0">
+              {dsc.description}
+            </h1>
+            <button className="flex-1 rounded-md h-7 bg-[#61dafb] mr-2">
+              <i className="fa-solid fa-edit"></i>
+            </button>
+            <button
+              className="flex-1 rounded-md h-7 bg-[#61dafb]"
+              onClick={() => deleteTodo(dsc.todos_id)}
+            >
+              <i className="fa-solid fa-trash"></i>
+            </button>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
 
 export default TodoLists;
--- src/components/TodosInputs.jsx
+++ src/components/TodosInputs.jsx
@@ -2,41 +2,54 @@ import { useState } from "react";
 import axios from "axios";
 
 const TodoInputs = () => {
-
   const [description, setDescription] = useState("");
-  const onSubmit = async e => {
+
+  const onSubmit = async (e) => {
     e.preventDefault();
+
     try {
-      const body = { description }
       const sendData = await axios({
-        method: 'POST',
-        url: 'http://localhost:4000/rest/add-todos',
+        method: "POST",
+        url: "http://localhost:4000/rest/add-todos",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
         data: {
-          description: description
-        }
-      })
+          description: description,
+        },
+      });
 
-      alert('Success add data!')
+      alert("Success add data!");
       console.log(sendData);
-      window.location = '/';
-    } catch (error) {
-      console.log(error);
+      window.location = "/";
+    } catch (err) {
+      console.error(err);
     }
-  }
+  };
 
   return (
     <>
       <form className="flex justify-center" method="post" onSubmit={onSubmit}>
         <div className="w-full flex flex-row md:w-2/5 lg:w-2/5 sm:w-80 p-3 h-15 rounded-xl bg-[#262626]">
-          <input type="text" placeholder="Type todo list here.." className="flex-0 text-white w-[75%] h-[2.27rem] p-3 rounded-md bg-transparent outline-none" value={description} onChange={e => setDescription(e.target.value)} required />
-          <button type="submit" className="flex-1 bg-[#61dafb] h-[2.27rem] rounded-xl text-slate-900" > Add</button>
+          <input
+            type="text"
+            placeholder="Type todo list here.."
+            className="flex-0 text-white w-[75%] h-[2.27rem] p-3 rounded-md bg-transparent outline-none"
+            value={description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+          <button
+            type="submit"
+            className="flex-1 bg-[#61dafb] h-[2.27rem] rounded-xl text-slate-900"
+          >
+            {" "}
+            Add
+          </button>
         </div>
       </form>
     </>
-  )
-}
+  );
+};
 
 export default TodoInputs;
