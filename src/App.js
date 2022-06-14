89553e5c9a413a3b6a5760a0ab904be4cb3d253d
Ramadandev <dan.alphadev@gmail.com>
2022-06-14 18:08:31 +0700

    add watermark

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import TodoInputs from "./components/TodosInputs";
 import TodoLists from "./components/TodoLists";
@@ -7,9 +6,12 @@ function App() {
   return (
     <>
       <div className="container p-6 bg-[#171717] w-full h-screen">
-        <h1 className="text-3xl text-[#61dafb] font-bold text-center m-7">
-          Todo list apps
+        <h1 className="text-3xl text-[#61dafb] font-bold text-center mt-7 mb-2">
+          Todo list apps by @mframadann
         </h1>
+        <h5 className="text-sm text-gray-400 mb-7 text-center">
+          Build with ReactJS - NodeJS - ExpressJS - PostgreSQL
+        </h5>
         <TodoInputs />
         <TodoLists />
       </div>
